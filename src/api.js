89d3aa4996fca1d6c168c2b96a40d4da0b3d565b
LambicJaune <thehopman@outlook.com>
2025-08-13 11:36:28 +0200

    implemented online/offline functionality

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 
 
 import mockData from './mock-data';
-
+import NProgress from 'nprogress';
 
 /**
  *
@@ -36,6 +36,12 @@ export const getEvents = async () => {
         return mockData;
     }
 
+    if (!navigator.onLine) {
+        const events = localStorage.getItem("lastEvents");
+        NProgress.done();
+        return events ? JSON.parse(events) : [];
+    }
+
     const token = await getAccessToken();
 
     if (token) {
@@ -44,6 +50,8 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
+            NProgress.done();
+            localStorage.setItem('lastEvents', JSON.stringify(result.events));
             return result.events;
         } else return null;
     }
