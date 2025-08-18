f54a3f8bae8f6170501628c06f25624af709eaf0
LambicJaune <thehopman@outlook.com>
2025-08-18 20:38:08 +0200

    Updated api.js

--- src/api.js
+++ src/api.js
@@ -100,8 +100,7 @@ const removeQuery = () => {
 const getToken = async (code) => {
     const encodeCode = encodeURIComponent(code);
     const response = await fetch(
-        'https://fpet8zsw47.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
-    );
+        'https://fpet8zsw47.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}');
     const { access_token } = await response.json();
     access_token && localStorage.setItem("access_token", access_token);
 
