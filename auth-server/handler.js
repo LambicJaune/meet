3958cf19c3389be7e734ca3b11ec5d1ad4b44f7e
LambicJaune <thehopman@outlook.com>
2025-08-18 20:27:39 +0200

    Revert "Updated handler.js"
    This reverts commit be49096c054f715bcaa7852571c0f1e6fe8fe26e.

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -16,10 +16,6 @@ const oAuth2Client = new google.auth.OAuth2(
     redirect_uris[0]
 );
 
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "https://meet-beta-rosy.vercel.app",
-  "Access-Control-Allow-Credentials": true,
-};
 
 module.exports.getAuthURL = async () => {
     /**
@@ -36,10 +32,12 @@ module.exports.getAuthURL = async () => {
     return {
         statusCode: 200,
         headers: {
-            'Access-Control-Allow-Origin': 'https://meet-beta-rosy.vercel.app',
+            'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Credentials': true,
         },
-        body: JSON.stringify({ authUrl }),
+        body: JSON.stringify({
+            authUrl,
+        }),
     };
 };
 
@@ -67,7 +65,7 @@ module.exports.getAccessToken = async (event) => {
             return {
                 statusCode: 200,
                 headers: {
-                    'Access-Control-Allow-Origin': 'https://meet-beta-rosy.vercel.app',
+                    'Access-Control-Allow-Origin': '*',
                     'Access-Control-Allow-Credentials': true,
                 },
                 body: JSON.stringify(results),
@@ -111,7 +109,7 @@ module.exports.getCalendarEvents = async (event) => {
         return {
             statusCode: 200,
             headers: {
-                'Access-Control-Allow-Origin': 'https://meet-beta-rosy.vercel.app',
+                'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Credentials': true,
             },
             body: JSON.stringify({ events: results.data.items })
