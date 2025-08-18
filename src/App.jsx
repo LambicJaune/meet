c8602964b5ee68f5c0c38dc11f09148d5070b3be
LambicJaune <thehopman@outlook.com>
2025-08-18 17:53:46 +0200

    Created EventGenresChart
    Also added a grid to display charts next to each other

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 import './App.css';
@@ -56,7 +57,10 @@ const App = () => {
                 currentNOE={currentNOE}
                 setCurrentNOE={setCurrentNOE}
                 setErrorAlert={setErrorAlert} />
-            <CityEventsChart allLocations={allLocations} events={events} />
+            <div className="charts-container">
+                <CityEventsChart allLocations={allLocations} events={events} />
+                <EventGenresChart events={events} />
+            </ div>
             <EventList events={events} />
         </div>
     );
