ed67f650ad2bf0b016263e0cb83ed468701c23e9
LambicJaune <thehopman@outlook.com>
2025-08-19 17:18:19 +0200

    Updated margin on EventGenresChart

--- src/components/EventGenresChart.jsx
+++ src/components/EventGenresChart.jsx
@@ -68,7 +68,7 @@ const EventGenresChart = ({ events }) => {
 
     return (
             <ResponsiveContainer width="99%" height={400}>
-                <PieChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+                <PieChart margin={{ top: 20, right: 20, bottom: 20, left: 0 }}>
                     <Pie
                         data={data}
                         dataKey="value"
