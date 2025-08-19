218b6264ff81f729f3bff08306b0b5eddf303ee2
LambicJaune <thehopman@outlook.com>
2025-08-19 17:10:26 +0200

    Revert "Fixed chart container width"
    This reverts commit 43efcea6d8c86965e9638c07a6d2aac501259c51.

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -22,7 +22,7 @@ const CityEventsChart = ({ allLocations, events }) => {
 
 
     return (
-        <ResponsiveContainer width="99%" minWidth={350} height={420}>
+        <ResponsiveContainer width="99%" height={420}>
             <ScatterChart
                 margin={{
                     top: 30,
